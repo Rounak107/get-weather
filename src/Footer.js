9e5185dd80a3d0564fd77cc11fe5a80a87cb1904
rounak <rounakbhuiya@gmail.com>
2022-11-23 17:53:09 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./index.css"
+
+function Footer(){
+    return (
+        <footer>
+            <div className="container">
+                <center>
+                    <p className="p-footer">Made by Rounak Bhuiya & Pradumm Tripath</p>
+                    <p className="p-footer">Weather is with you</p>
+                </center>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer
+
